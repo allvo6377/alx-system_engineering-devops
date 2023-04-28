0854db80a1fb9747238fcd849c150ec4da7e3f87
allvo6377 <alvinpilot@gmail.com>
2023-04-28 21:41:46 +0000

    Betty fix

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,31 +4,43 @@
 #include <unistd.h>
 
 /**
+ * infinite_while - infinite loop
+ *
+ * Return: 0 on success
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
  * main - creates 5 zombie processes
  *
- * Return: always 0
+ * Return: 0 on success
  */
 int main(void)
 {
-    pid_t pid;
-    int i;
+	pid_t child_pid;
+	int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        pid = fork();
-        if (pid > 0)
-        {
-            printf("Zombie process created, PID: %d\n", pid);
-            sleep(1);
-        }
-        else
-            exit(0);
-    }
-
-    /* Keep the parent process running */
-    while (1)
-        sleep(1);
-
-    return (0);
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+			sleep(1);
+		}
+		else
+		{
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
 }
 
